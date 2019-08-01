683c18bc75a52d462abdded029710b7b250e70a3
tdietzen <51529950+tdietzen@users.noreply.github.com>
2019-08-01 18:50:54 +0200

    Add LC-ISCLP processing in main.m

--- main.m
+++ main.m
@@ -6,23 +6,25 @@
 %
 % If you find it useful, please cite:
 %
-% [1] T. Dietzen, S. Doclo, M. Moonen, and T. van Waterschoot, ?Integrated
+% [1] T. Dietzen, S. Doclo, M. Moonen, and T. van Waterschoot, "Integrated
 % sidelobe cancellation and linear prediction Kalman filter for joint
 % multimicrophone dereverberation, interfering speech cancellation, and
-% noise reduction,? ESAT-STADIUS Tech. Rep. TR 19-70, KU Leuven, Belgium,
+% noise reduction," ESAT-STADIUS Tech. Rep. TR 19-70, KU Leuven, Belgium,
 % submitted for publication, June 2019.
-% [2] T. Dietzen, S. Doclo, M. Moonen, and T. van Waterschoot, ?Square
+% [2] T. Dietzen, "Spatio-Temporal Speech enhancement in adverse acoustic
+% conditions," PhD thesis, KU Leuven, Belgium, 2019, to appear.
+% [3] T. Dietzen, S. Doclo, M. Moonen, and T. van Waterschoot, "Square
 % rootbased multi-source early PSD estimation and recursive RETF update in
-% reverberant environments by means of the orthogonal Procrustes problem,?
+% reverberant environments by means of the orthogonal Procrustes problem,"
 % ESAT-STADIUS Tech. Rep. TR 19-69, KU Leuven, Belgium, submitted for
 % publication, June 2019.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Example of the ISCLP Kalman filter as described in [1]. The code in
-% contained main.m requests an SNR vaule by the user, loads microphone
-% signals, estimates early PSDs and updates RETFs using [2] (see also
-% https://github.com/tdietzen/SQRT-PSD-RETF), runs the ISCLP Kalman filter,
-% and plays back the enhanced signal.
+% Example of the ISCLP Kalman filter and its low-complexity variants as
+% described in [1,2]. The code in contained main.m requests an SNR vaule by
+% the user, loads microphone signals, estimates early PSDs and updates
+% RETFs using [3] (see also https://github.com/tdietzen/SQRT-PSD-RETF),
+% runs the ISCLP Kalman filter, and plays back the enhanced signal.
 
 
 %% PREAMBLE
@@ -74,7 +76,7 @@ N_STFT_half = floor(N_STFT/2)+1;
 % frequency vector
 f = linspace(0,fs/2,N_STFT_half);
 
-%%% ISCLP Kalman filter [1]
+%%% ISCLP Kalman filter [1,2]
 % prediction length
 L = 6;
 % forgetting factor alpha
@@ -95,7 +97,7 @@ end
 % gain decay limitation
 beta_SCLP_KF = db2mag(-2);
 
-%%% PSD estimation and RETF update [2]
+%%% PSD estimation and RETF update [3]
 % forgetting factor zeta
 zeta  = tau2forget(2*M*R_STFT/fs, R_STFT, fs);
 % laplace coefficients of speech per frequency bin
@@ -134,9 +136,9 @@ plotSpec(s1_STFT(:,:,1), 'mag', xTickProp, yTickProp, cRange, 0); title('s1'); y
 drawnow;
 
 
-%% EARLY PSD ESTIMATION, RETF UPDATE [2]
+%% EARLY PSD ESTIMATION, RETF UPDATE [3]
 
-fprintf(' * estimate early PSDs, update RETFs [2]...\n');
+fprintf(' * estimate early PSDs, update RETFs [3]...\n');
 
 % correlation matrix of microphone signal
 Psi_y_STFT = estim_corrmat(y_STFT, zeta);
@@ -164,72 +166,88 @@ plotSpec(phi_s_hat(:,:,1), 'pow', xTickProp, yTickProp, cRange, 0); title(['phi 
 drawnow;
 
 
-%% ISCLP KALMAN FILTER [1]
-
-fprintf(' * run ISCLP Kalman filer [1]...\n');
+%% ISCLP KALMAN FILTER [1,2]
 
 % number of frames
 numFrames = size(y_STFT,2);
 
 % init outputs
-q_STFT              = zeros(N_STFT_half, numFrames);
-e_prio_STFT         = zeros(N_STFT_half, numFrames);
-e_post_STFT         = zeros(N_STFT_half, numFrames);
-e_post_smooth_STFT  = zeros(N_STFT_half, numFrames);
+q_STFT                   = cell(1,4);
+e_prio_STFT              = cell(1,4);
+e_post_STFT              = cell(1,4);
+e_post_smooth_STFT       = cell(1,4);
+q_STFT(1,:)              = {zeros(N_STFT_half, numFrames)};
+e_prio_STFT(1,:)         = {zeros(N_STFT_half, numFrames)};
+e_post_STFT(1,:)         = {zeros(N_STFT_half, numFrames)};
+e_post_smooth_STFT(1,:)  = {zeros(N_STFT_half, numFrames)};
 
+for variantIdx = 1:4
+    
+    switch variantIdx
+        case 1
+            complexity = 'L2M2';
+        case 2
+            complexity = 'LM2';
+        case 3
+            complexity = 'L2M';
+        case 4
+            complexity = 'LM';
+    end
+    fprintf([' * run O(' complexity ')-cost ISCLP Kalman filer [1,2]...\n']);
 
-for k = 2:N_STFT_half
-    
-    % reorganize data
-    y_stack      = shiftdim(squeeze(y_STFT(k,:,:)),1);                % from (1 x numFrames x M)       to (M x numFrames)
-    h_stack      = shiftdim(squeeze(H_hat_post_STFT(k,:,:,1)),1);     % from (1 x numFrames x M x 1)   to (M x numFrames)
-    psi_sT_stack = shiftdim(squeeze(phi_s_hat(k,:,1)),1);             % from (1 x numFrames x 1)       to (1 x numFrames)
-    
-    % run ISCLP Kalman filter and spectral post processor
-    [ q_stack, ...
-        e_prio_stack,...
-        e_post_stack,...  
-        e_post_smooth_stack ] = ...
-        ISCLP(...
-        A, Psi_w_delta{k},...
-        y_stack,...
-        psi_sT_stack,...
-        h_stack,...
-        Psi_w_tilde_init{k},...
-        beta_SCLP_KF );
-    
-    % save output
-    q_STFT(k,:)               = q_stack;
-    e_prio_STFT(k,:)          = e_prio_stack;
-    e_post_STFT(k,:)          = e_post_stack;
-    e_post_smooth_STFT(k,:)   = e_post_smooth_stack;
-    
+    for k = 2:N_STFT_half
+        
+        % reorganize data
+        y_stack      = shiftdim(squeeze(y_STFT(k,:,:)),1);                % from (1 x numFrames x M)       to (M x numFrames)
+        h_stack      = shiftdim(squeeze(H_hat_post_STFT(k,:,:,1)),1);     % from (1 x numFrames x M x 1)   to (M x numFrames)
+        psi_sT_stack = shiftdim(squeeze(phi_s_hat(k,:,1)),1);             % from (1 x numFrames x 1)       to (1 x numFrames)
+        
+        % run ISCLP Kalman filter and spectral post processor
+        [ q_stack, ...
+            e_prio_stack,...
+            e_post_stack,...
+            e_post_smooth_stack ] = ...
+            ISCLP(...
+            A, Psi_w_delta{k},...
+            y_stack,...
+            psi_sT_stack,...
+            h_stack,...
+            Psi_w_tilde_init{k},...
+            beta_SCLP_KF,...
+            complexity);
+        
+        % save output
+        q_STFT{variantIdx}(k,:)               = q_stack;
+        e_prio_STFT{variantIdx}(k,:)          = e_prio_stack;
+        e_post_STFT{variantIdx}(k,:)          = e_post_stack;
+        e_post_smooth_STFT{variantIdx}(k,:)   = e_post_smooth_stack;
+        
+    end
+
+    figure('Name',['enhanced signal, O(' complexity ')-cost, SNR = ' num2str(SNR) 'dB']);
+    plotSpec(e_post_smooth_STFT{variantIdx}, 'mag', xTickProp, yTickProp, cRange, 0); title(['e post, O(' complexity ')-cost, SNR = ' num2str(SNR) 'dB']); ylabel('f/kHz');
+    drawnow;
 end
-
-% plot
-figure('Name',['enhanced signal, beta = 0, SNR = ' num2str(SNR) 'dB']);
-plotSpec(e_post_STFT(:,:,1),        'mag', xTickProp, yTickProp, cRange, 0); title(['e post, beta = 0, SNR = ' num2str(SNR) 'dB']); ylabel('f/kHz');
-figure('Name',['enhanced signal, beta = ' num2str(round(beta_SCLP_KF,2)) ', SNR = ' num2str(SNR) 'dB']);
-plotSpec(e_post_smooth_STFT(:,:,1), 'mag', xTickProp, yTickProp, cRange, 0); title(['e post, beta = ' num2str(round(beta_SCLP_KF,2)) ', SNR = ' num2str(SNR) 'dB']); ylabel('f/kHz');
-drawnow;
-
 
 %% ISTFT PROCESSING
 
-e_post_TD        = calc_ISTFT(e_post_STFT, win, N_STFT, R_STFT, 'onesided');
-e_post_smooth_TD = calc_ISTFT(e_post_smooth_STFT, win, N_STFT, R_STFT, 'onesided');
-
+e_post_smooth_TD = cell(1,4);
+for variantIdx = 1:4
+    e_post_smooth_TD{variantIdx} = calc_ISTFT(e_post_smooth_STFT{variantIdx}, win, N_STFT, R_STFT, 'onesided');
+end
 
 %% WRITE AUDIO AND PLAY BACK
 
 audiowrite(['.' filesep 'audio' filesep 'v_SNR_' num2str(SNR) 'dB.wav'], v_TD_SNR_scaled, fs);
 audiowrite(['.' filesep 'audio' filesep 'y_SNR_' num2str(SNR) 'dB.wav'], y_TD, fs);
-audiowrite(['.' filesep 'audio' filesep 'e_post_SNR_' num2str(SNR) 'dB.wav'],e_post_TD,fs);
-audiowrite(['.' filesep 'audio' filesep 'e_post_smooth_SNR_' num2str(SNR) 'dB.wav'],e_post_smooth_TD,fs);
+audiowrite(['.' filesep 'audio' filesep 'e_post_smooth_O(L2M2)_SNR_' num2str(SNR) 'dB.wav'],e_post_smooth_TD{1},fs);
+audiowrite(['.' filesep 'audio' filesep 'e_post_smooth_O(LM2)_SNR_' num2str(SNR) 'dB.wav'],e_post_smooth_TD{2},fs);
+audiowrite(['.' filesep 'audio' filesep 'e_post_smooth_O(L2M)_SNR_' num2str(SNR) 'dB.wav'],e_post_smooth_TD{3},fs);
+audiowrite(['.' filesep 'audio' filesep 'e_post_smooth_O(LM)_SNR_' num2str(SNR) 'dB.wav'],e_post_smooth_TD{4},fs);
 
 fprintf(' * play microphone signal...\n'); 
 sound(y_TD(:,1), fs); pause(length(y_TD(:,1))/fs);
-fprintf(' * play enhanced signal...\n'); 
-sound(e_post_smooth_TD(:,1), fs);
+fprintf(' * play enhanced signal, O(LM)-cost...\n'); 
+sound(e_post_smooth_TD{4}(:,1), fs);
 
 fprintf('\nDONE.\n'); 
