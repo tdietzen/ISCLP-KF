278030ac216bf5d81950a95d4d860cdfa9ecc4ad
tdietzen <51529950+tdietzen@users.noreply.github.com>
2021-01-14 15:33:01 +0100

    Update main.m

--- main.m
+++ main.m
@@ -145,6 +145,21 @@ Psi_y_STFT = estim_corrmat(y_STFT, zeta);
     'lambdaMin', 0,...
     'forgetPSD', zeta);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% NOTE: instead of desmoothing eigenvalues as in the implementation above,
+% one might prefer to use instantaneous eigenvalues, see also [a] and the
+% desmooth_GEVD() function in [b].
+%
+% [a] T. Dietzen, M. Moonen, and T. van Waterschoot, 'Instantaneous PSD
+% estimation for speech enhancement based on generalized principal
+% components,' in Proc. 28th European Signal Process. Conf. (EUSIPCO 2020),
+% Amsterdam, Netherlands, Jan 2021, pp. 1-5.
+%
+% [b] https://github.com/tdietzen/INSTANT-PSD
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 [phi_s_hat,...
     phi_xl_hat,...
     H_hat_prior_STFT,...
