ccfb8bb92f79a1593a62ff7c512b9cc7cfcf0310
tdietzen <51529950+tdietzen@users.noreply.github.com>
2021-01-14 15:34:22 +0100

    Update desmooth_GEVD.m

--- tools/estim/SQRT_PSD_RETF/desmooth_GEVD.m
+++ tools/estim/SQRT_PSD_RETF/desmooth_GEVD.m
@@ -99,6 +99,22 @@ for k=2:N_FT_half
         %
         % save
         lambda_STFT(k,l,:) = shiftdim(lambda, -2);
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %
+        % NOTE: instead of desmoothing eigenvalues as in the implementation
+        % above, one might prefer to use instantaneous eigenvalues, see
+        % also [a] and the desmooth_GEVD() function in [b].
+        %
+        % [a] T. Dietzen, M. Moonen, and T. van Waterschoot, 'Instantaneous
+        % PSD estimation for speech enhancement based on generalized
+        % principal components,' in Proc. 28th European Signal Process.
+        % Conf. (EUSIPCO 2020), Amsterdam, Netherlands, Jan 2021, pp. 1-5.
+        %
+        % [b] https://github.com/tdietzen/INSTANT-PSD
+        %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
     end               
 end
 
